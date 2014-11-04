f4edf00a48c63fc678c78aeb3bea7faf1d4cc593
Rowandish <paolo.guglielmini.vr@gmail.com>
2014-11-05 02:05:41 +0900

    Added support for video retargeting

---
+++ examples/video_example.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+from os.path import basename, splitext
+import numpy as np
+import time
+import cv2
+from seamcarving import seam_carving, progress_bar
+from video_helper import save_video_caps
+from image_helper import local_path
+
+progress_bar(False)
+
+
+def generate_step(I, img):
+  result = np.empty((img.shape[0], img.shape[1], img.shape[2]))
+  r = np.arange(img.shape[2])
+  for i in xrange(img.shape[0]):
+    result[i] = r > np.vstack(I[i])
+  return result.astype(np.uint64)
+
+def print_seams(result, seams):
+  print "printing seam"
+  seams = seams.astype(np.uint64)
+  A = np.zeros_like(result)
+  correction = np.zeros((result.shape[0], result.shape[1], result.shape[2])).astype(np.uint64)
+  for i in xrange(seams.shape[0]):
+    X, Y = np.mgrid[:result.shape[0], :result.shape[1]]
+    I = seams[i]
+    I = I + correction[X, Y, I]
+    color = np.random.rand(3) * 255
+    A[X, Y, I] = color
+    correction = correction + generate_step(I, result)
+  return A
+
+deleteNumberW = 1
+counting_frames = 100
+filename = '../assets/car.m4v'
+suffix = ''
+
+makeNewDecData = False
+
+debug = False
+saveBMP = True
+
+cap = cv2.VideoCapture(local_path(filename))
+size = '_reduce' if deleteNumberW < 0 else '_enlarge'
+size += str(-deleteNumberW) if deleteNumberW < 0 else str(deleteNumberW)
+
+name = splitext(basename(filename))[0] + suffix + '_' + size + '_' + str(int(time.time()))
+
+frames_count, fps, width, height = cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT), cap.get(cv2.cv.CV_CAP_PROP_FPS), cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH), cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+
+frames_count = frames_count if counting_frames is None else counting_frames
+
+video = np.empty((frames_count, height, width, 3))
+
+i = 0
+while cap.isOpened() and i < frames_count:
+  ret, X = cap.read()
+  if not ret:
+    break
+  y = cv2.cvtColor(X, cv2.COLOR_BGR2YCR_CB)
+  y = y.astype(np.float64)
+  video[i] = X
+  i += 1
+
+result, seams = seam_carving(video, deleteNumberW)
+
+A = print_seams(video, seams)
+A = np.clip(A * 0.8 + video, 0, 255).astype(np.uint8)
+
+result = np.clip(result, 0, 255).astype(np.uint8)
+save_video_caps(result, 'results/' + name + '_')
+save_video_caps(A, 'results/' + name + '_seams_')
+cap.release()
+print 'Finished file: ' + basename(filename)
---
+++ tests/graphcut_design.py
@@ -0,0 +1,42 @@
+import numpy as np
+from seamcarving.video_reduce import video_seam_carving_decomposition
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def plot_graph_3d(graph, nodes_shape, plot_terminal=True, plot_weights=True, font_size=7):
+    w_h = nodes_shape[1] * nodes_shape[2]
+    X, Y = np.mgrid[:nodes_shape[1], :nodes_shape[2]]
+    aux = np.array([Y.ravel(), X[::-1].ravel()]).T
+    positions = {i: aux[i] for i in xrange(w_h)}
+
+    for i in xrange(1, nodes_shape[0]):
+        for j in xrange(w_h):
+            positions[w_h * i + j] = [positions[j][0] + 0.3 * i, positions[j][1] + 0.2 * i]
+
+    positions['s'] = np.array([-1, nodes_shape[1] / 2.0 - 0.5])
+    positions['t'] = np.array([nodes_shape[2] + 0.2 * nodes_shape[0], nodes_shape[1] / 2.0 - 0.5])
+
+    nxg = graph.get_nx_graph()
+    if not plot_terminal:
+        nxg.remove_nodes_from(['s', 't'])
+
+    nx.draw(nxg, pos=positions)
+    nx.draw_networkx_labels(nxg, pos=positions)
+    if plot_weights:
+        edge_labels = dict([((u, v,), d['weight'])
+                     for u, v, d in nxg.edges(data=True)])
+        nx.draw_networkx_edge_labels(nxg, 
+                                     pos=positions,
+                                     edge_labels=edge_labels,
+                                     label_pos=0.3,
+                                     font_size=font_size)
+    plt.axis('equal')
+    plt.show()
+
+Image = (np.ones((2, 5, 5)) * 3).astype(np.uint64)  # np.arange(50).reshape(2, 5, 5)
+
+subject = video_seam_carving_decomposition(Image, 0, 0, False)
+g, nodeids = subject.generate_graph(Image)
+
+plot_graph_3d(g, nodeids.shape)
